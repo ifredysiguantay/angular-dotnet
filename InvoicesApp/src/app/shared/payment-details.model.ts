8e5950e23b2404f9712bb3f200241bdb5a37f89b
ifredyorlando <tooheyred@gmail.com>
2021-08-27 10:23:50 -0600

    Actualizacion en el modelo de facturas

--- InvoicesApp/src/app/shared/payment-details.model.ts
+++ InvoicesApp/src/app/shared/payment-details.model.ts
@@ -3,6 +3,9 @@ import { DecimalPipe } from "@angular/common";
 export class PaymentDetails {
     id:number=0;
     nombre:string='';
+    nit:string='';
+    fecha:Date=new Date();;
+    correlativo:string='';
     producto:string='';
     precio:number=0;
     cantidad:number=0;
